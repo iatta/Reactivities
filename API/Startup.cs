194343cafd99366ca01b4ca067221183eb0ee482
islam atta` <islamattaelkady@gmail.com>
2019-11-25 22:08:43 +0300

    End of Seciton 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,12 @@ namespace API
             services.AddDbContext<DataContext>(opt =>{
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy => {
+                        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             
             services.AddControllers();
         }
@@ -48,6 +54,7 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
